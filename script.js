18838c6dfdad343a8de093913a69c88fe3592d29
nedimburo <nedimb9402@gmail.com>
2023-08-10 16:36:32 +0200

    Created function for populating display with books

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let myLibrary = [];
+let myLibrary = [
+    {
+        "title": "Don Quixote",
+        "author": "Miguel de Cervantes",
+        "pages": 304,
+        "read": true
+    },
+    {
+        "title": "A Tale of Two Cities",
+        "author": "Charles Dickens",
+        "pages": 290,
+        "read": false
+    },
+    {
+        "title": "The Lord of the Rings",
+        "author": "J.R.R. Tolkien",
+        "pages": 1220,
+        "read": true
+    }
+];
 
 function Book() {
   // the constructor...
@@ -6,4 +25,24 @@ function Book() {
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+window.addEventListener("load", updateDisplay);
+
+function updateDisplay(){
+    let displayContainer=document.getElementById("container");
+    displayContainer.innerHTML="";
+    for (let i=0; i<myLibrary.length; i++){
+        let sample=`<div class="book-item">
+                        <h2>Title: ${myLibrary[i].title}</h2>
+                        <p>Author: ${myLibrary[i].author}</p>
+                        <p>Number of pages ${myLibrary[i].pages}</p>
+                        <p>Status: ${(myLibrary[i].read) ? "Read" : "Not read yet"}</p>
+                        <div class="item-button-container">
+                            <button>Change status</button>
+                            <button>Delete book</button>
+                        </div>
+                    </div>`;
+        displayContainer.innerHTML+=sample;
+    }
 }
