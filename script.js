3313469f0021ccf8bbfb5a157b4495a90d58dc97
nedimburo <nedimb9402@gmail.com>
2023-08-10 19:38:38 +0200

    Created functions for changing book status and deleting

--- script.js
+++ script.js
@@ -34,13 +34,13 @@ function updateDisplay(){
     displayContainer.innerHTML="";
     for (let i=0; i<myLibrary.length; i++){
         let sample=`<div class="book-item">
-                        <h2>Title: ${myLibrary[i].title}</h2>
+                        <h2>Title: "${myLibrary[i].title}"</h2>
                         <p>Author: ${myLibrary[i].author}</p>
                         <p>Number of pages ${myLibrary[i].pages}</p>
                         <p id="read_status_${i}">Status: ${(myLibrary[i].read) ? "Read" : "Not read yet"}</p>
                         <div class="item-button-container">
-                            <button class="book-button change" onclick="changeReadStatus(${i})">Change status</button>
-                            <button class="book-button delete" onclick="deleteBook(${i})">Delete book</button>
+                            <button class="book-button change" onclick="changeReadStatus(${i})">Change Status</button>
+                            <button class="book-button delete" onclick="deleteBook(${i})">Delete Book</button>
                         </div>
                     </div>`;
         displayContainer.innerHTML+=sample;
@@ -48,9 +48,18 @@ function updateDisplay(){
 }
 
 function changeReadStatus(index){
-
+    let targetedBookStatus=document.getElementById(`read_status_${index}`);
+    if (myLibrary[index].read){
+        targetedBookStatus.innerHTML="Status: Not read yet";
+        myLibrary[index].read = false;
+    }
+    else{
+        targetedBookStatus.innerHTML="Status: Read";
+        myLibrary[index].read = true;
+    }
 }
 
 function deleteBook(index){
-    
+    myLibrary.splice(index, 1);
+    updateDisplay();
 }
